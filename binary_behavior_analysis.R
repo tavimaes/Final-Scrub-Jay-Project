8f2973718d540c194e0d72e48f46f51cd0775bf2
tavimaes <tmaes@ucsc.edu>
2026-03-14 14:52:24 -0700

    finished graphing all four binary behaviors

--- binary_behavior_analysis.R
+++ binary_behavior_analysis.R
@@ -12,6 +12,7 @@ library(performance)
 library(lme4)
 library(dplyr)
 library(broom.mixed)
+library(ggdist)
 
 
 #read in clean data
@@ -217,151 +218,148 @@ model_output_tibble <- bind_rows(model_output_tibble, interest_hypotenuse_est,
                                 interest_est, interest_playback_est)
 
 # FLEE occurrence model
-m.flee <- glmer(FLEE ~ SPECIES + TREATMENT + GROUP.SIZE + HYPOTENUSE +
-                  SEASON +
+m.flee <- glmer(FLEE ~ SPECIES*TREATMENT + GROUP.SIZE + HYPOTENUSE + PLAYBACK +
+                  (1 | SUBSITE),
+                data = sjdf_clean,
+                family = binomial)
+
+check_collinearity(m.flee)  # check collinearity between predictors
+
+sim.m.flee <- simulateResiduals(fittedModel = m.flee) # check model fit
+testDispersion(sim.m.flee)
+testZeroInflation(sim.m.flee)
+testOutliers(sim.m.flee)
+
+summary(m.flee)  # get model output
+
+# save playback summary stats for FLEE
+flee_playback_est <- tidy(m.flee) |> 
+  filter(term == "PLAYBACKYES") |> 
+  mutate(model = "FLEE")
+
+# rerun model without PLAYBACK if not significant, include SEASON
+m.flee <- glmer(FLEE ~ SPECIES*TREATMENT + GROUP.SIZE + HYPOTENUSE + SEASON +
                   (1 | SUBSITE),
                 data = sjdf_clean,
                 family = binomial)
 
 check_collinearity(m.flee)
-
 sim.m.flee <- simulateResiduals(fittedModel = m.flee)
 testDispersion(sim.m.flee)
 testZeroInflation(sim.m.flee)
 testOutliers(sim.m.flee)
 
 summary(m.flee)
-flee_hypotenuse_est <- tidy(m.flee) |> filter(term == "HYPOTENUSE")
 
-m.flee <- glmer(FLEE ~ SPECIES + TREATMENT + GROUP.SIZE +
-                  SEASON +
+flee_hypotenuse_est <- tidy(m.flee) |> 
+  filter(term == "HYPOTENUSE") |> 
+  mutate(model = "FLEE")
+
+# rerun final model without HYPOTENUSE if not significant
+m.flee <- glmer(FLEE ~ SPECIES*TREATMENT + GROUP.SIZE + SEASON +
                   (1 | SUBSITE),
                 data = sjdf_clean,
                 family = binomial)
 
+check_collinearity(m.flee)
+sim.m.flee <- simulateResiduals(fittedModel = m.flee)
+testDispersion(sim.m.flee)
+testZeroInflation(sim.m.flee)
+testOutliers(sim.m.flee)
+
+summary(m.flee)
+
+# get all other stats from final model
 flee_est <- tidy(m.flee) |> mutate(model = "FLEE")
 
-model_output_tibble <- bind_rows(model_output_tibble, flee_est, 
-                                 flee_hypotenuse_est)
-
-
-# tidy tibble up
-model_output_tibble$model[7] <- "ALARM"
-model_output_tibble$model[14] <- "MOB"
-model_output_tibble$model[21] <- "INTEREST"
-model_output_tibble$model[28] <- "FLEE"
-
-model_output_tibble <- model_output_tibble |> 
-  select(-effect, - group) |>
-  filter(term != "(Intercept)") |> 
-  filter(term != "sd__(Intercept)")
-  
-model_output_tibble <- model_output_tibble |> select(model, everything())
-
-# save as a file 
-write.csv(model_output_tibble, "binary_behav_stats.csv")
+model_output_tibble <- bind_rows(model_output_tibble, flee_hypotenuse_est, 
+                                 flee_est, flee_playback_est)
 
 
 # GRAPH building
 
+library(ggbeeswarm)
 
-# Generate predictions for each model
-
-# Create a function to get predicted probabilities for a model
-get_pred <- function(model, response_name) {
-  # SPECIES varies, TREATMENT fixed at "hawk", other covariates averaged
-  #(including weighted mean of SEASON)
-  emm <- emmeans(model, ~ SPECIES, at = list(TREATMENT = "HAWK"))
+ggplot() +
   
-  # Convert to data frame with predicted probabilities and 95% CI
-  emm_df <- summary(emm, type = "response", infer = TRUE) |> 
-    as.data.frame() |> 
-    mutate(response = response_name)
+  # Replace stat_slab with quasirandom points
+  geom_quasirandom(
+    data = sj_long,
+    aes(
+      x = SPECIES,
+      y = VALUE,
+      color = TREATMENT,
+      group = TREATMENT
+    ),
+    dodge.width = 0.6,  
+    width = 0.2,
+    size = 1,
+    alpha = 0.1
+  ) +
   
-  return(emm_df)
-}
-
-# Apply to all four models
-pred_alarm    <- get_pred(m.alarm, "ALARM")
-pred_mob      <- get_pred(m.mob, "MOB")
-pred_interest <- get_pred(m.interest, "INTEREST")
-pred_flee     <- get_pred(m.flee, "FLEE")
-
-# Combine all into one table
-predictions_table <- bind_rows(pred_alarm, pred_mob, pred_interest, pred_flee)
-
-# View
-predictions_table
-
-
-# graphing time
-predictions_table <- predictions_table |> 
-  mutate(panel_label = factor(response, 
-                              levels = c("Alarm", "Mob", "Interest", "FLEE"),
-                              labels = c("a)", "b)", "c)", "d)")))
-predictions_table <- predictions_table |>
-  filter(response != "FLEE")
-
-
-
-
-predictions_table <- predictions_table %>%
-  mutate(response = recode(response,
-                           "ALARM" = "Alarm",
-                           "MOB" = "Mob",
-                           "INTEREST" = "Interest"))
-
-sjdf_issj <- sjdf_clean |>
-  filter(TREATMENT == "HAWK") |>
-  filter(SPECIES == "ISSJ")
-
-sjdf_casj <- sjdf_clean |>
-  filter(TREATMENT == "HAWK") |>
-  filter(SPECIES == "CASJ")
-
-issj_alarm_mean <- mean(sjdf_issj$ALARM)
-casj_alarm_mean <- mean(sjdf_casj$ALARM)
-issj_mob_mean <- mean(sjdf_issj$MOB)
-casj_mob_mean <- mean(sjdf_casj$MOB)
-issj_interest_mean <- mean(sjdf_issj$INTEREST)
-casj_interest_mean <- mean(sjdf_casj$INTEREST)
-
-raw_data <- data.frame(
-  response = c("Alarm", "Alarm", "Mob","Mob", "Interest", "Interest"), 
-  mean = c(issj_alarm_mean, casj_alarm_mean, issj_mob_mean,
-           casj_mob_mean, issj_interest_mean, casj_interest_mean),
-  SPECIES = c("ISSJ", "CASJ", "ISSJ", "CASJ", "ISSJ", "CASJ")
-)
-
-sig_df <- data.frame(
-  response = c("Alarm", "Mob", "Interest"),  
-  SPECIES = c("ISSJ", "ISSJ", "ISSJ"),
-  y = c(0.68, 0.03, 0.28),
-  label = c("*", "*", "*")
-)
-
-
-
-ggplot(predictions_table, aes(x = response, y = prob, shape = SPECIES, 
-                              group = SPECIES, color = SPECIES)) +
-  geom_point(data = raw_data, aes(x = response, y = mean), 
-             show.legend = FALSE,
-             color = "darkgray",
-             position = position_nudge(x = 0.2),
-             size = 4) +
-  geom_point(size = 4) +
-  geom_errorbar(aes(ymin = asymp.LCL, ymax = asymp.UCL), width = 0.2, 
-                linewidth = 0.75, show.legend = FALSE) +
-  geom_text(data = sig_df, aes(x = response, y = y, label = label),
-            inherit.aes = FALSE, size = 6) +
-  labs(x = "Behaviour", y = "Predicted probability", color = NULL, shape = NULL) +
-  scale_shape_manual(values = c("ISSJ" = 16, "CASJ" = 1)) +
+  # Predicted points
+  geom_point(
+    data = pred_df,
+    aes(
+      x = SPECIES,
+      y = prob,
+      color = TREATMENT,
+      shape = TREATMENT
+    ),
+    size = 3,
+    position = position_dodge(width = 0.6)
+  ) +
+  
+  # Predicted error bars
+  geom_errorbar(
+    data = pred_df, show.legend = FALSE,
+    aes(
+      x = SPECIES,
+      ymin = asymp.LCL,
+      ymax = asymp.UCL,
+      color = TREATMENT
+    ),
+    width = 0.3,
+    position = position_dodge(width = 0.6)
+  ) +
+  
+  # Facet labels
+  geom_text(
+    data = label_df,
+    aes(x = x, y = y, label = label),
+    inherit.aes = FALSE,
+    hjust = 0,
+    vjust = 0,
+    size = 5
+  ) +
+  
+  facet_wrap(~ factor(BEHAVIOR, levels = c("ALARM", "MOB", "INTEREST", "FLEE")),
+             ncol = 2) +
+  
+  scale_color_manual(values = c("CONTROL" = "gray60",
+                                "HAWK" = "darkblue")) +
+  scale_fill_manual(values = c("CONTROL" = "gray60",
+                               "HAWK" = "darkblue")) +
+  scale_shape_manual(values = c("CONTROL" = 15,
+                                "HAWK" = 17)) +
+  
+  scale_x_discrete(labels = c(
+    CASJ = expression(italic("A. californica")),
+    ISSJ = expression(italic("A. insularis"))
+  )) +
+  
+  scale_y_continuous(limits = c(0,1), expand = c(0.02,0.05)) +
+  
   theme_classic(base_size = 14) +
-  scale_color_manual(values = c("red", "darkblue")) +
-  scale_x_discrete(limits = c("Alarm", "Mob", "Interest"))
-  
+  labs(x = "Species", y = "Probability") +
+  theme(strip.text = element_blank(),
+        strip.background = element_blank(),
+        panel.border = element_rect(color = "black", fill = NA))
+
+ggsave("fig1.png", last_plot(), width = 7, height = 5, dpi = 300)
 
 
-ggsave("fig1.png", last_plot(), width = 8, height = 5, dpi = 300)
+
+
 
 
